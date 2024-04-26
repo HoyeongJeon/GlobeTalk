34ca230e3c19ebe4e16194c9ccca04b79987ff15
HoyeongJeon <hoyeong0302@gmail.com>
2024-04-27 01:14:51 +0900

    fix: cors 정책 추가

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // 요청 데이터를 해당 타입으로 변환
