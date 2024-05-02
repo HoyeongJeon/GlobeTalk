8f50c5c4e3b81fd61d4b11b0cc86022cd0eccafc
HoyeongJeon <hoyeong0302@gmail.com>
2024-05-02 12:43:53 +0900

    feat: 채팅 수락시 채팅방 생성

--- src/chats/entities/chat.entity.ts
+++ src/chats/entities/chat.entity.ts
@@ -1,11 +1,12 @@
 import { BaseModel } from 'src/common/entities/base.entity';
 import { MessageModel } from 'src/messages/entities/messages.entity';
 import { UserModel } from 'src/users/entities/user.entity';
-import { Entity, ManyToMany, OneToMany } from 'typeorm';
+import { Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 
 @Entity('chats')
 export class ChatModel extends BaseModel {
   @ManyToMany(() => UserModel, (user) => user.Chats)
+  @JoinTable()
   Users: UserModel[];
 
   @OneToMany(() => MessageModel, (message) => message.Chat)
