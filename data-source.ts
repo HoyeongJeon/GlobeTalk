4f8dabf696102e9d1e17b70e5f7c5ce9937b65fa
HoyeongJeon <hoyeong0302@gmail.com>
2024-06-22 16:29:58 +0900

    🐳: docker 환경변수 변경

--- data-source.ts
+++ data-source.ts
@@ -8,11 +8,11 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'mysql',
-  host: configService.get('MYSQL_HOST'),
-  port: configService.get<number>('MYSQL_PORT'),
-  username: configService.get('MYSQL_USERNAME'),
-  password: configService.get('MYSQL_PASSWORD'),
-  database: configService.get('MYSQL_DATABASE'),
+  host: configService.get('DOCKER_MYSQL_HOST'),
+  port: configService.get<number>('DOCKER_MYSQL_PORT'),
+  database: configService.get('DOCKER_MYSQL_DATABASE'),
+  username: configService.get('DOCKER_MYSQL_USERNAME'),
+  password: configService.get('DOCKER_MYSQL_PASSWORD'),
   synchronize: false,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/database/migrations/*.ts'],
